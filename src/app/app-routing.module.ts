c71084eae2b36fba88acc3f8b97e79f424f77aef
Artem Buhera <artem.bugera@gmail.com>
2021-11-29 20:18:27 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { TaskTableComponent } from "./pages/task-table/task-table.component";
 import { TaskExecutorTableComponent } from "./pages/task-executor-table/task-executor-table.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'configs', pathMatch: 'full', },
-  { path: 'configs', component: TaskTableComponent },
+  { path: '', redirectTo: 'tasks', pathMatch: 'full', },
+  { path: 'tasks', component: TaskTableComponent },
   { path: 'executors', component: TaskExecutorTableComponent },
-  { path: '**', redirectTo: 'configs' }
+  { path: '**', redirectTo: 'tasks' }
 ];
 
 @NgModule({

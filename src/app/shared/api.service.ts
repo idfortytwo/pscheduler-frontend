14223f3a7b13bdd4f528a69fd24c2ca8fc9d1881
Artem Buhera <artem.bugera@gmail.com>
2021-12-09 21:51:13 +0100

    removed redundant inner typing

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
-import {ExecutionLog, ExecutionOutputLog, Task, TaskExecutor} from "./data-models";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { ExecutionLog, ExecutionOutputLog, Task, TaskExecutor } from "./data-models";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -12,56 +12,56 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<{'tasks': Task[]}> {
-    return this.http.get<{'tasks': Task[]}>(
+    return this.http.get<any>(
        this.baseUrl + '/task')
   }
 
   getTask(taskID: number): Observable<{'task': Task}> {
-    return this.http.get<{'task': Task}>(
+    return this.http.get<any>(
        this.baseUrl + '/task/' + taskID)
   }
 
   addTask(task: Task): Observable<{'task_id': number}> {
-    return this.http.post<{'task_id': number}>(
+    return this.http.post<any>(
       this.baseUrl + '/task/', task)
   }
 
   deleteTask(taskID: number): Observable<{'task_id': number}> {
-    return this.http.delete<{'task_id': number}>(
+    return this.http.delete<any>(
        this.baseUrl + '/task/' + taskID)
   }
 
   editTask(taskID: number, task: Task): Observable<{'task_id': number}> {
-    return this.http.post<{'task_id': number}>(
+    return this.http.post<any>(
       this.baseUrl + '/task/' + taskID, task)
   }
 
   getTaskExecutors(): Observable<{'task_executors': TaskExecutor[]}> {
-    return this.http.get<{'task_executors': TaskExecutor[]}>(
+    return this.http.get<any>(
        this.baseUrl + '/executor')
   }
 
   runExecutor(taskID: number): Observable<{'task_id': number}> {
-    return this.http.post<{'task_id': number}>(
+    return this.http.post<any>(
        this.baseUrl + '/run_executor/' + taskID, {})
   }
 
   stopExecutor(taskID: number): Observable<{'task_id': number}> {
-    return this.http.post<{'task_id': number}>(
+    return this.http.post<any>(
        this.baseUrl + '/stop_executor/' + taskID, {})
   }
 
-  getExecutionLogs(): Observable<{ 'execution_logs': ExecutionLog[], 'last_execution_log_id': number }> {
-    return this.http.get<{ 'execution_logs': ExecutionLog[], 'last_execution_log_id': number }>(
+  getExecutionLogs(): Observable<{'execution_logs': ExecutionLog[], 'last_execution_log_id': number}> {
+    return this.http.get<any>(
       this.baseUrl + '/execution_log'
     )
   }
 
-  getExecutionOutputLogs(executionLogID: number, lastExecutionOutputLogID: number): Observable<{
-    'execution_output_logs': ExecutionOutputLog[], 'last_execution_output_log_id': number }> {
+  getExecutionOutputLogs(executionLogID: number, lastExecutionOutputLogID: number):
+        Observable<{'execution_output_logs': ExecutionOutputLog[], 'last_execution_output_log_id': number}> {
     let params = new HttpParams().set('last_execution_output_log_id', lastExecutionOutputLogID)
 
-    return this.http.get<{ 'execution_output_logs': ExecutionOutputLog[], 'last_execution_output_log_id': number }>(
+    return this.http.get<any>(
       this.baseUrl + '/execution/output/' + executionLogID,
       { params: params }
     )
